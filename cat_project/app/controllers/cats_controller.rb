08f1fb2018fe89168572c66d56bd7cafd558f158
Numero Uno Colin Mackenzie and Rusty 'The Deuce' Toms <jonathantoms5@gmail.com>
2013-09-11 14:22:07 -0400

    adds edit, update, create views

--- cat_project/app/controllers/cats_controller.rb
+++ cat_project/app/controllers/cats_controller.rb
@@ -11,8 +11,39 @@ class CatsController < ApplicationController
   end
 
   def new
+    @params = {}
     @colors = Cat.cat_colors
     render :new
   end
 
+  def edit
+    @cat = Cat.find(params[:id])
+    @params = {name: @cat.name, age: @cat.age, birth_date: @cat.birth_date,               color: @cat.color, sex: @cat.sex}
+    @colors = Cat.cat_colors
+    render :edit
+  end
+
+  def update
+    @params = params[:cat]
+    @cat = Cat.find(params[:id])
+    @colors = Cat.cat_colors
+    if @cat.update_attributes(@params)
+      @cats = Cat.all
+      redirect_to cats_url
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @params = params[:cat]
+    @cat = Cat.new(params[:cat])
+    @colors = Cat.cat_colors
+    if @cat.save
+      @cats = Cat.all
+      redirect_to cats_url
+    else
+      render :new
+    end
+  end
 end
