3837819540bea15ea822e0afa33be89ed991878f
Jayan et Clement <jfk303@nyu.edu clementpigeon@gmail.com>
2013-09-11 08:58:46 -0400

    first commit

---
+++ cat_project/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CatProject::Application.config.secret_token = 'a45affa45e95578d8d778778fcc232c7fee2a15e7c1fa76a69ba120b4e9b71ec06b25785a9d090dbe89e7bec034e906f59436110f579e869c083bec1bc0a7368'
